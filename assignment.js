06c3acea615ba9af58268ce9a76627727ecedf28
komal jha <kjha4951@gmail.com>
2023-07-14 12:06:59 +0530

    go

---
+++ assignment.js
@@ -0,0 +1,52 @@
+//ternary oprator
+
+// var a=51
+// var b=(a>50 ? "get pension" : "not get pension")
+// console.log(b)
+
+//Comparison Operators
+  
+
+// var a=10;
+// var b=10;
+// if(a==b){
+//     console.log("equal")
+// }
+// else {
+//     console.log("not equal")
+// }
+
+
+
+//assignment oprator
+// let s=7;
+// let b=4;
+// s+=b;
+// console.log(s)
+// b-=s;
+// console.log(b)
+
+
+//comparision oprator
+
+// let num1=20
+// let num2=30
+
+// console.log(num1==num2)
+// console.log(num1===num2)
+// console.log(num1 != num2)
+// console.log(num1 > num2)
+
+
+//string oprator
+
+let str1="hello"
+let str2=" world"
+
+console.log(str1+str2);
+
+
+
+
+
+
