04455d27bb06ddbfef46b8944ba6c165ba51efc9
kjha4951 <128771901+kjha4951@users.noreply.github.com>
2023-07-19 00:23:54 -0700

    assignmnet3

---
+++ assignmnet3.js
@@ -0,0 +1,41 @@
+let str="123";
+let num=Number(str)
+console.log(typeof num);
+
+
+
+// let num=123;
+// let str=String(num)
+// console.log(typeof str);
+
+
+// let json = '{"name":"komal","id":351}';
+// let jon = JSON.parse(json);
+// console.log(jon.name);
+// console.log(jon.id); 
+// console.log(typeof jon); 
+
+
+// let bool = true;
+// let str = String(bool);
+// console.log(str);
+// console.log(typeof str); 
+
+
+
+// let str= "false";
+// let bool = Boolean(str);
+// console.log(bool); // Output: true
+// console.log(typeof bool);
+
+
+
+// let strnum = "456";
+// let int = parseInt(strnum);
+// console.log(int); 
+// console.log(typeof int);
+
+// let num = 456.44;
+// let flo = parseFloat(num);
+// console.log(flo); 
+// console.log(typeof flo);
